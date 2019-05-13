20601da3f87f2dac48e3bdfb9ccac4c912038fdd
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-13 11:01:05 -0600

    updates search controller with station_count local

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
+    facade = Facade.new(params[:q])
     render locals: {
-      stations: Facade.new
+      station_count: facade.station_count,
+      stations: facade.get_stations
     }
   end
 end
