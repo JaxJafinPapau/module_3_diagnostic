362a20f8ee351797ec2277802ef5e76979d5159c
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-13 09:27:50 -0600

    Initial commit creates test for search feature on root page

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,28 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+  context "and fill in the search form with 80206" do
+    scenario "and click Locate" do
+      visit "/"
+      fill_in "q", with: "80206"
+
+      click_on "Locate"
+
+      expect(current_path).to eq("/search")
+      expect(page).to have_content("Stations: 90")
+      expect(page).to have_css(".station", count: 15)
+      within first(".station") do
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Address:")
+        expect(page).to have_content("Fuel Types:")
+        expect(page).to have_content("Distance:")
+        expect(page).to have_content("Access Times:")
+      end
+      # Then I should see the total results of the stations that match my query, 90.
+      # Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+      # And the stations should be limited to Electric and Propane
+      # And the stations should only be public, and not private, planned or temporarily unavailable.
+      # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    end
+  end
 end
